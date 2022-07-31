fbbc7481083f3ced3f5e8379df09eea9931cd7b5
rusiruavb <rusiruavb98@gmail.com>
2022-07-31 14:26:49 +0530

    Get max prediction value

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import json
 import os
 import cv2
+import requests
 from typing import List
 from fastapi import FastAPI, File, UploadFile
 from config.firebase import firebase
 from firebase_admin import firestore, credentials, initialize_app
+from PIL import Image
 
 app = FastAPI()
 storage = firebase.storage()
@@ -24,7 +26,7 @@ class Link:
 async def root():
   return {"message": "Hello world 2"}
 
-@app.post("/upload")
+@app.post("/make-frame")
 async def uploadVideo(files: List[UploadFile] = File(...)):
   links = []
   for i, file in enumerate(files):
@@ -52,24 +54,6 @@ async def uploadVideo(files: List[UploadFile] = File(...)):
   except Exception as err:
     return {"error": str(err)}
 
-  return {"message": links}    
-
-@app.get("/get-files")
-async def get_links():
-  try:
-    file_links = []
-    docs = db.collection(u"file_links").stream()
-    
-    for doc in docs:
-      data = { "id": doc.id, "file": doc.to_dict() }
-      file_links.append(data)
-
-    return file_links
-  except Exception as err:
-    return { "error": str(err) }
-
-@app.post("/frame")
-async def make_frame():
   try:
     file_links = []
     docs = db.collection(u"file_links").stream()
@@ -95,10 +79,10 @@ def create_dir(path):
   except OSError:
     print(f"ERROR: creating directory with name {path}")
 
-def save_frame(path, file_name, dir, gap=10):
+def save_frame(path, file_name, dir, gap=50):
   save_path = os.path.join(dir, file_name)
   create_dir(save_path)
-
+  url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/67eb6bde-6855-4c6b-b555-006f273e9239/detect/iterations/Iteration1/image"
   cap = cv2.VideoCapture(path)
   idx = 0
   
@@ -111,8 +95,53 @@ def save_frame(path, file_name, dir, gap=10):
 
     if idx == 0:
       cv2.imwrite(f"{save_path}/{idx}.png", frame)
+      with open(f"{save_path}/{idx}.png", "rb") as file:
+        headers = {'content-type': 'application/octet-stream', 'Prediction-Key': '154db301ad6844bca3dfcf20cf246a61'}
+        response = requests.post(url, data=file, headers=headers)
+        data = json.loads(response.text)
+        predictions = data["predictions"]
+        maxProbability = 0
+        tagName = ""
+        for pred in predictions:
+          if (pred["probability"] > maxProbability):
+            maxProbability = pred["probability"]
+            tagName = pred["tagName"]
+        
+        print(maxProbability)
+        print(tagName)
+        # print(data["predictions"])
+        os.unlink(f"{save_path}/{idx}.png")
     else:
-      if idx % gap == 0:
+      if idx % gap == 0: # 1 % 50 = 0
         cv2.imwrite(f"{save_path}/{idx}.png", frame)
+        with open(f"{save_path}/{idx}.png", "rb") as file:
+          headers = {'content-type': 'application/octet-stream', 'Prediction-Key': '154db301ad6844bca3dfcf20cf246a61'}
+          response = requests.post(url, data=file, headers=headers)
+          data = json.loads(response.text)
+          predictions = data["predictions"]
+          maxProbability = 0
+          tagName = ""
+          for pred in predictions:
+            if (pred["probability"] > maxProbability):
+              maxProbability = pred["probability"]
+              tagName = pred["tagName"]
+          
+          print(maxProbability)
+          print(tagName)
+          # print(data["predictions"])
+          os.unlink(f"{save_path}/{idx}.png")
 
     idx += 1
+
+  # api call -> /
+  # https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/67eb6bde-6855-4c6b-b555-006f273e9239/detect/iterations/Iteration1/image
+  # Set Prediction-Key Header to : 154db301ad6844bca3dfcf20cf246a61
+  # Set Content-Type Header to : application/octet-stream
+  # Set Body to : <image file>
+
+
+# blob = bucket.blob(f"{save_path}/{idx}.png", chunk_size=262144*10)
+#       blob.upload_from_filename(f"{save_path}/{idx}.png")
+#       blob.make_public()
+#       link = blob.public_url
+#       print(link)
