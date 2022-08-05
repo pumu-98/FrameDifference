b54c6979b6a32e234995b68c6fefa1ac8d82ba18
rusiruavb <rusiruavb98@gmail.com>
2022-08-06 00:17:05 +0530

    Fix database reference issue

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import os
 import cv2
 import requests
-import pymysql
+import mysql.connector
 from typing import List
 from fastapi import FastAPI, File, UploadFile
 from config.firebase import firebase
@@ -17,6 +17,20 @@ default_app = initialize_app(cred)
 db = firestore.client()
 file_ref = db.collection("file_links")
 
+# Database connection
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="RavB1998",
+  database="frames_db"
+)
+
+cursor = db.cursor(buffered=True)
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS frames_db")
+cursor.execute("CREATE TABLE IF NOT EXISTS files (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(300) NULL, link VARCHAR(1000), status ENUM('PENDING', 'COMPLETED') NOT NULL DEFAULT 'PENDING', PRIMARY KEY (id))")
+cursor.execute("CREATE TABLE IF NOT EXISTS predicted_data (id INT NOT NULL AUTO_INCREMENT, tag_name VARCHAR(255), max_probability DECIMAL(20,10), video_id INT, PRIMARY KEY (id), FOREIGN KEY (video_id) REFERENCES files(id))")
+
 class Link:
   def __init__(self, id, link, name):
     self.id = id
@@ -25,12 +39,11 @@ class Link:
 
 @app.get("/")
 async def root():
-  return {"message": "Hello world 2"}
+  return {"message": "Hello world"}
 
 @app.post("/make-frame")
 async def uploadVideo(files: List[UploadFile] = File(...)):
   links = []
-  con = pymysql.connect(host="localhost",user="root",password="",db="drug_alcohol")
   for i, file in enumerate(files):
     try:
       contents = await file.read()
@@ -51,47 +64,39 @@ async def uploadVideo(files: List[UploadFile] = File(...)):
   
   try:
     for link in links:
-     
-      # data = {"link": link.link, "name": link.name}
-      # file_ref.document().set(data)
-       
-      name = link.name
-      link= link.link
-      cursor=con.cursor()
-      cursor.execute("insert into files(name,link) value(%s,%s)", (name,link))
-      con.commit()
-      print(name)
-
+      query ="INSERT INTO files (name, link) VALUES (%s, %s)"
+      data = (link.name, link.link)
+      cursor.execute(query, data)
+      db.commit()
+      print("Video data recorded")
   except Exception as err:
     return {"error": str(err)}
 
   try:
-    file_links = []
-    # docs = db.collection(u"file_links").stream()
-     
-    cursor=con.cursor()
-    cursor.execute("select id, name, link from files")
-    docs = cursor.fetchall()
+    query = "SELECT * FROM files WHERE status='PENDING'"
+    cursor.execute(query)
+    columns = cursor.description
+    results = []
 
-    #print(docs)
-    
-    for doc in docs:
-      itemArr = list(doc)
-      print(itemArr[0])
-      print(itemArr[1])
-      print(itemArr[2])
+    for value in cursor.fetchall():
+      temp = {}
+      for (index, column) in enumerate(value):
+        temp[columns[index][0]] = column
+      results.append(temp)
+    print(results)
 
-
-      data = { "id": doc.id, "file": doc.to_dict() }
-      file_links.append(data)
-
-    for link in file_links:
-      path = link["file"]["link"]
-      file_name = link["file"]["name"]
+    for link in results:
+      video_id= link["id"],
+      path = link["link"]
+      file_name = link["name"]
       save_dir = "frames"
-      save_frame(path, file_name, save_dir)
+      video_id = str(video_id).strip("'(,)'")
+      save_frame(video_id, path, file_name, save_dir)
+      query = "UPDATE files SET status = 'COMPLETED' WHERE id=" + video_id
+      cursor.execute(query)
+      db.commit()
 
-    return file_links
+    return results
   except Exception as err:
     return { "error": str(err) }
 
@@ -102,8 +107,7 @@ def create_dir(path):
   except OSError:
     print(f"ERROR: creating directory with name {path}")
 
-def save_frame(path, file_name, dir, gap=50):
-  con = pymysql.connect(host="localhost",user="root",password="",db="drug_alcohol")
+def save_frame(video_id, path, file_name, dir, gap=50):
   save_path = os.path.join(dir, file_name)
   create_dir(save_path)
   url = "https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/67eb6bde-6855-4c6b-b555-006f273e9239/detect/iterations/Iteration1/image"
@@ -112,7 +116,6 @@ def save_frame(path, file_name, dir, gap=50):
   
   while True:
     ret, frame = cap.read()
-    # print(ret)
     if ret == False:
       cap.release()
       break
@@ -127,21 +130,24 @@ def save_frame(path, file_name, dir, gap=50):
         maxProbability = 0
         tagName = ""
         for pred in predictions:
-          
           if (pred["probability"] > maxProbability):
             maxProbability = pred["probability"]
             tagName = pred["tagName"]
         
-        print(maxProbability)
-        cursor=con.cursor()
-        cursor.execute("insert into predicted_data(tagName,maxProbability) value(%s,%s)", (tagName,maxProbability))
-        con.commit()
-        print(tagName)
+        # print('%s %f' % (tagName, maxProbability))
+        print(f"{video_id} - {tagName} {maxProbability}")
+        maxProbability = str(maxProbability)
+        query = "INSERT INTO predicted_data (tag_name, max_probability, video_id) VALUES(%s, %s, %s)"
+        data = (tagName, maxProbability, video_id)
+        try:
+          cursor.execute(query, data)
+          db.commit()
+        except Exception as err:
+          print(err)
         file.close()
-        # print(data["predictions"])
         os.unlink(f"{save_path}/{idx}.png")
     else:
-      if idx % gap == 0: # 1 % 50 = 0
+      if idx % gap == 0:
         cv2.imwrite(f"{save_path}/{idx}.png", frame)
         with open(f"{save_path}/{idx}.png", "rb") as file:
           headers = {'content-type': 'application/octet-stream', 'Prediction-Key': '154db301ad6844bca3dfcf20cf246a61'}
@@ -151,59 +157,53 @@ def save_frame(path, file_name, dir, gap=50):
           maxProbability = 0
           tagName = ""
           for pred in predictions:
-            
-            
             if (pred["probability"] > maxProbability):
               maxProbability = pred["probability"]
               tagName = pred["tagName"]
-          
 
-           
-          
-          
-          
-
-          print(maxProbability)
-          cursor=con.cursor()
-          cursor.execute("insert into predicted_data(tagName,maxProbability) value(%s,%s)", (tagName,maxProbability))
-          con.commit()
-          print(tagName)
+          # print('%s %f' % (tagName, maxProbability))
+          print(f"{video_id} - {tagName} {maxProbability}")
+          maxProbability = str(maxProbability)
+          query = "INSERT INTO predicted_data (tag_name, max_probability, video_id) VALUES(%s, %s, %s)"
+          data = (tagName, maxProbability, video_id)
+          try:
+            cursor.execute(query, data)
+            db.commit()
+          except Exception as err:
+            print(err)
           file.close()
-          # print(data["predictions"])
           os.unlink(f"{save_path}/{idx}.png")
-
     idx += 1
-  con.close()
 
 
 @app.get("/pred")
 async def getPredictions():
-  con = pymysql.connect(host="localhost",user="root",password="",db="drug_alcohol")
-  cursor=con.cursor()
-  cursor.execute("select * from predicted_data")
-  result = cursor.fetchall()
-
-  print(result)
-
-  return result
+  query = "SELECT tag_name, max_probability, video_id FROM predicted_data"
   
+  cursor.execute(query)
+  columns = cursor.description
+  results = []
 
+  for value in cursor.fetchall():
+    temp = {}
+    for (index, column) in enumerate(value):
+      temp[columns[index][0]] = column
+    results.append(temp)
 
-   
-    
+  return results
 
+@app.get("/pred/{id}")
+async def getPredictionsForVideo(id: str):
+  query = "SELECT tag_name, max_probability FROM predicted_data WHERE video_id=" + id
+  
+  cursor.execute(query)
+  columns = cursor.description
+  results = []
 
-  # api call -> /
-  # https://eastus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/67eb6bde-6855-4c6b-b555-006f273e9239/detect/iterations/Iteration1/image
-  # Set Prediction-Key Header to : 154db301ad6844bca3dfcf20cf246a61
-  # Set Content-Type Header to : application/octet-stream
-  # Set Body to : <image file>
+  for value in cursor.fetchall():
+    temp = {}
+    for (index, column) in enumerate(value):
+      temp[columns[index][0]] = column
+    results.append(temp)
 
-
-# blob = bucket.blob(f"{save_path}/{idx}.png", chunk_size=262144*10)
-#       blob.upload_from_filename(f"{save_path}/{idx}.png")
-#       blob.make_public()
-#       link = blob.public_url
-#       print(link)
-
- 
+  return results
